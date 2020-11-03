53052743e20c09c87876875178e7ddc90dcbbfbc
Mateusz00 <mateusz.gis00@gmail.com>
2020-11-03 11:54:23 +0100

    stage 7

---
+++ MvcMusicStore/Models/Genre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Models
+{
+    public class Genre
+    {
+        [DisplayName("Genre")]
+        public int GenreId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Album> Albums { get; set; }
+    }
+}
